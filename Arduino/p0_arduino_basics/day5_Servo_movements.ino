bdf04c72eb324d0f6dafc0b5331a6927f7c93247
Ashok Suthar <ashoksuthar1069@gmail.com>
2026-05-16 08:30:56 +0530

    Adding control of angle change to avoid jitters

--- Arduino/p0_arduino_basics/day5_Servo_movements.ino
+++ Arduino/p0_arduino_basics/day5_Servo_movements.ino
@@ -6,6 +6,8 @@ int potPin=A0;
 int potVal;
 int angle;
 
+int oldangle=0;
+
 void setup() {
   // put your setup code here, to run once:
 myServo.attach(9);//attach Pin 9 to Servo Signal
@@ -18,9 +20,15 @@ void loop() {
 
 potVal=analogRead(potPin); //Reads from 0 to 1023
 
+// Serial.println(potVal);
+
 angle=map(potVal,0,1023,0,180); //map Potentiometer value to angle
 
+if (abs(angle-oldangle)>2) //making a move only if there is a input change for more than 2* to avoid noise
+{
 myServo.write(angle);
+oldangle=angle;
+}
 
 Serial.print("Pot : ");
 Serial.print(potVal);
@@ -29,4 +37,5 @@ Serial.print(angle);
 Serial.print("\n");
 
 delay(20);
+
 }
