e665318edff7bab5140ea1b349962cbc5a9138d9
Ashok Suthar <ashoksuthar1069@gmail.com>
2026-05-21 20:47:08 +0530

    Adding to and fro 0-180-0 movement

--- Arduino/p4_servo_radar_scanner/code/p4_servo_radar_scanner.ino
+++ Arduino/p4_servo_radar_scanner/code/p4_servo_radar_scanner.ino
@@ -12,10 +12,39 @@ Servo myservo;
 const int trig=10;//trig Pin will never change
 const int echo=11;//echo Pin will never change
 
+const int ledPin=12; //LED Pin
+
 long duration; //Duration for sound to go and return back
 float distance; //Distance measured by Ultrasonic
 
-float avgDist; //Averaging out distance to reduce jitters
+float medDist; //Median distance to reduce jitters
+
+int angle = 0; //Servo angle => 0 to 180
+int direction = 1; //Servo direction => State (-1/+1)
+
+float calculateMedian(float arr[], int size)
+{
+    // Sort the array in ascending order
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                float temp = arr[j]; //assign higher value to temp
+                arr[j] = arr[j + 1]; //move lower value at higher value's place
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    
+    // Check if the array size is even or odd
+    if (size % 2 == 0) {
+        return (arr[(size / 2) - 1] + arr[size / 2]) / 2.0; // Average of two middle elements
+    } else {
+        return arr[size / 2]; // Exact middle element
+    }
+}
 
 
 void setup()
@@ -24,19 +53,18 @@ void setup()
     Serial.begin(9600);
     pinMode(trig, OUTPUT);
     pinMode(echo, INPUT);
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, LOW);
 }
 
 void loop()
 {
-myservo.write(0);//Set it to zero angle at the beginning
-delay(500); // Give the servo time to reach the starting position
 
+    // Move servo to current angle
+    myservo.write(angle);
 
-for (int i=0;i<180;i++)
-{
-    myservo.write(i);
-    avgDist=0;
-
+    // Measure Distance
+    float readings[5]; // Array to store 5 readings
     for (int j=0; j<5; j++)
     {
         digitalWrite(trig, LOW);
@@ -51,18 +79,34 @@ for (int i=0;i<180;i++)
         duration=pulseIn(echo, HIGH, 30000); // Added 30ms timeout to prevent freezing
         distance=duration*0.034/2; //Conversion of sound travel time to distance
 
-        avgDist+=distance;
+        readings[j] = distance;
+        delay(100);
     }
 
-    avgDist=avgDist/5;
+    medDist = calculateMedian(readings, 5);
     Serial.print("Angle: ");
-    Serial.print(i);
+    Serial.print(angle);
     Serial.print(" Distance: ");
-    Serial.print(avgDist);
+    Serial.print(medDist);
     Serial.print("\n");
-    delay(100);
-}
-    // Trap execution in an infinite loop so it doesn't run again
-    while (true) {
+
+    //Typical range of ultrasonic sensor is 4m, so I will define
+    //if distance is less than 50cm then there is an obstacle
+    //RED LED On
+
+    if (medDist>5 && medDist<50)
+    {
+        digitalWrite(ledPin,HIGH);
+    }
+    else
+    {
+        digitalWrite(ledPin, LOW); // Turn off LED when clear
+    }
+
+    // Update Servo Angle for next loop iteration
+    angle = angle + direction;
+    if (angle >= 180 || angle <= 0)
+    {
+        direction = -direction; // Change direction at extreme points
     }
 }
